b1b35e3e29a005485635863a688c31abe5475854
andrewblais <anb2015@gmail.com>
2024-04-26 18:45:58 -0400

    Initial commit and push.

--- README.md
+++ README.md
@@ -1,8 +1,169 @@
-# Created in completing an assignment for Angela Yu Course:
+# OpenAndAlign
 
-## **Day 98, Professional Portfolio Project [Python Automation]**
+A class to automate opening your favorite Windows applications and aligning them on the desktop. Created for
+Professional Portfolio Project: Assignment 17: Angela Yu 100 Days of Code -- "Python Automation: Custom Automation".
 
-### **_Assignment 17: "Custom Automation"_**
+This project is fairly simple and small in scope, but solves very directly and effectively a task which I've been
+seeking to automate for some time now, i.e. opening up my dual Firefox windows to either side of the screen in an exact
+and organized manner. Not a big deal, by any means, but very helpful and fun to watch work.
+
+I've often wondered if there was a way to achieve this kind of functionality via a Firefox extension, but haven't found
+one that opens windows to a pre-ordained configuration with this kind of precision. Perhaps future versions can include
+an extension...
+
+I used PyAutoGUI on a previous project to automate the Google Dinosaur Game, and I'd heard of and browsed Al Sweigart's
+famous book, [Automate the Boring Stuff with Python](https://automatetheboringstuff.com/) before, but this is the first
+time I've I put two and two together and realized that this is his library. Kudos to him -- such a useful and fun
+module!
+
+### Resources Utilized
+
+- [Python](https://www.python.org/)
+
+- [pywin32](https://github.com/mhammond/pywin32) (`win32gui` in imports)
+
+_MIT License: Copyright (c) 2024- Andrew Blais_
+
+---
+
+### Future Updates
+
+- Provide functionality to utilize across Linux and Mac as well.
+
+- Clarify and improve math and algorithms involved in determining different and best screen coordinates for app windows.
+
+- more...
+
+---
+
+### Documentation:
+
+```requirements
+PyAutoGUI>=0.9.54
+pywin32>=306
+```
+
+_Docstrings for `main.py`:
+
+```
+Help on class OpenAndAlign in module __main__:
+
+class OpenAndAlign(builtins.object)
+ |  OpenAndAlign(app_name: str, image_file_name: str, desired_win_loc: list = [0, 0], align_right: bool = False, confidence: float = 0.9)
+ |
+ |  A class to automate the process of opening an application and aligning its window.
+ |
+ |  :param app_name: The name of the application to open via the Start Menu.
+ |  :type app_name: str
+ |  :param image_file_name: The file name of the image to be located on the screen.
+ |                          User creates and stores this image file in the 'static'
+ |                          project folder.
+ |  :type image_file_name: str
+ |  :param desired_win_loc: The desired window location coordinates. Defaults to (0, 0).
+ |  :type desired_win_loc: list, optional
+ |  :param align_right: Whether to align the window to the right side of the screen.
+ |                      Defaults to False.
+ |  :type align_right: bool, optional
+ |  :param confidence: The confidence threshold for image recognition. Defaults to 0.9.
+ |  :type confidence: float, optional
+ |
+ |  :ivar app_name: The name of the application to open via the Start Menu.
+ |  :type app_name: str
+ |  :ivar image_file_name: The file name of the image to be located on the screen.
+ |                         User creates and stores this image file in the 'static'
+ |                         project folder.
+ |  :type image_file_name: str
+ |  :ivar desired_win_loc: The desired window location coordinates. Defaults to (0, 0).
+ |  :type desired_win_loc: list
+ |  :ivar align_right: Whether to align the window to the right side of the screen.
+ |                     Defaults to False.
+ |  :type align_right: bool
+ |  :ivar screen_w: The width of the screen.
+ |  :type screen_w: int
+ |  :ivar app_left: The left coordinate of the located application window.
+ |  :type app_left: int
+ |  :ivar app_top: The top coordinate of the located application window.
+ |  :type app_top: int
+ |  :ivar corner_avoid_val: The value to avoid the corner of the screen.
+ |  :type corner_avoid_val: int
+ |  :ivar right_adjust_val: The value to adjust for aligning the window to the right side.
+ |  :type right_adjust_val: int
+ |  :ivar confidence: The confidence threshold for image recognition. Defaults to 0.9.
+ |  :type confidence: float
+ |
+ |  Methods defined here:
+ |
+ |  __init__(self, app_name: str, image_file_name: str, desired_win_loc: list = [0, 0], align_right: bool = False, confidence: float = 0.9)
+ |      Initialize self.  See help(type(self)) for accurate signature.
+ |
+ |  app_drag(self, desired_win_locations: list) -> None
+ |      Moves mouse cursor to upper left of app window and drags to desired location.
+ |
+ |      :param desired_win_locations: The desired window location coordinates.
+ |
+ |      :return: None
+ |
+ |  app_locate(self, image_file_name: str) -> None
+ |      Locates the specified image on the screen. User must crop a screenshot an image
+ |       representing the upper-left part of the app to identify the app to pyautogui.
+ |      Store this image in the project 'static' folder.
+ |
+ |      :param image_file_name: The file name of the image.
+ |      :type image_file_name: str
+ |
+ |      :return: None
+ |
+ |  app_open(self, app_name: str) -> None
+ |      Opens the specified application via the Start Menu.
+ |
+ |      :param app_name: The name of the application.
+ |      :type app_name: str
+ |
+ |      :return: None
+ |
+ |  right_move(self) -> int
+ |      Adjusts values based on window width so window will be snug against right
+ |       side of computer screen.
+ |
+ |      :return: The adjusted x-coordinate.
+ |      :rtype: int | float
+ |
+ |  run_program(self) -> None
+ |      Runs the program and all relevant class methods to open the application and
+ |       align its window.
+ |
+ |      :return: None
+ |
+ |  screen_w_val(self) -> None
+ |      Gets the width of the screen.
+ |
+ |      :return: None
+ |
+ |  ----------------------------------------------------------------------
+ |  Static methods defined here:
+ |
+ |  error_handling(exception: Exception) -> None
+ |      Static method to handle errors that might arise.
+ |
+ |      :return: None
+ |
+ |  ----------------------------------------------------------------------
+ |  Data descriptors defined here:
+ |
+ |  __dict__
+ |      dictionary for instance variables
+ |
+ |  __weakref__
+ |      list of weak references to the object
+```
+
+---
+
+## Created in completing an assignment for Angela Yu Course 100 Days of Code: The Complete Python Pro Bootcamp.
+
+### **Day 98, Professional Portfolio Project [Python Automation]**
+
+#### **_Assignment 17: "Custom Automation"_**
 
 Automate some aspect of your life using Python.
 
--- main.py
+++ main.py
@@ -1,68 +1,191 @@
-# import keyboard
-import os
 import pyautogui as pg
-from pyautogui import ImageNotFoundException
+import sys
 import time
+from win32gui import GetForegroundWindow, GetWindowRect  # noqa
 
 
-# Prerequisites:
-#  close firefox previous time so window is width you like
-# Recommend the user take a screenshot to find upper part (left or right) and use this
-#  in `locateOnScreen()` pg method.
+class OpenAndAlign:
+    """
+    A class to automate the process of opening an application and aligning its window.
 
-class OpenFirefoxWindows:
-    def __init__(self):
-        self.screen_dim = None
-        self.screen_w = None
-        self.screen_h = None
-        self.firefox_left = None
-        self.firefox_top = None
-        self.screenshot = None
+    :param app_name: The name of the application to open via the Start Menu.
+    :type app_name: str
+    :param image_file_name: The file name of the image to be located on the screen.
+                            User creates and stores this image file in the 'static'
+                            project folder.
+    :type image_file_name: str
+    :param desired_win_loc: The desired window location coordinates. Defaults to (0, 0).
+    :type desired_win_loc: list, optional
+    :param align_right: Whether to align the window to the right side of the screen.
+                        Defaults to False.
+    :type align_right: bool, optional
+    :param confidence: The confidence threshold for image recognition. Defaults to 0.9.
+    :type confidence: float, optional
 
-    def get_monitor_dimensions(self):
-        try:
-            self.screen_w, self.screen_h = pg.size()
-        except (AttributeError, IndexError, Exception) as error:
-            print(error)
+    :ivar app_name: The name of the application to open via the Start Menu.
+    :type app_name: str
+    :ivar image_file_name: The file name of the image to be located on the screen.
+                           User creates and stores this image file in the 'static'
+                           project folder.
+    :type image_file_name: str
+    :ivar desired_win_loc: The desired window location coordinates. Defaults to (0, 0).
+    :type desired_win_loc: list
+    :ivar align_right: Whether to align the window to the right side of the screen.
+                       Defaults to False.
+    :type align_right: bool
+    :ivar screen_w: The width of the screen.
+    :type screen_w: int
+    :ivar app_left: The left coordinate of the located application window.
+    :type app_left: int
+    :ivar app_top: The top coordinate of the located application window.
+    :type app_top: int
+    :ivar corner_avoid_val: The value to avoid the corner of the screen.
+    :type corner_avoid_val: int
+    :ivar right_adjust_val: The value to adjust for aligning the window to the right side.
+    :type right_adjust_val: int
+    :ivar confidence: The confidence threshold for image recognition. Defaults to 0.9.
+    :type confidence: float
+    """
+
+    def __init__(self,
+                 app_name: str,
+                 image_file_name: str,
+                 desired_win_loc: list = [0, 0],  # noqa
+                 align_right: bool = False,
+                 confidence: float = .9):
+        self.app_name = app_name
+        self.image_file_name = image_file_name
+        self.desired_win_loc = desired_win_loc
+        self.align_right = align_right
+        self.screen_w = 1920
+        self.app_left = 0
+        self.app_top = 0
+        self.corner_avoid_val = 10
+        self.right_adjust_val = 15
+        self.confidence = confidence
+
+        self.run_program()
 
     @staticmethod
-    def firefox_one_open():
-        pg.press('win')
-        time.sleep(1)
-        pg.write('firefox')
-        time.sleep(1)
-        pg.press('enter')
-        time.sleep(3)
+    def error_handling(exception: Exception) -> None:
+        """
+        Static method to handle errors that might arise.
 
-    def firefox_one_locate(self):
+        :return: None
+        """
+        print(f"Unexpected error: {exception}. Discretely exiting program...")
+        sys.exit(1)
+
+    def screen_w_val(self) -> None:
+        """
+        Gets the width of the screen.
+
+        :return: None
+        """
         try:
-            # Box(left=0, top=0, width=319, height=86)
-            img_path = os.path.abspath(
-                os.curdir) + "/static/firefox_locate.png"
-            firefox_locator = pg.locateOnScreen(img_path)
-            self.firefox_left = firefox_locator.left + 8
-            self.firefox_top = firefox_locator.top + 8
-        except ImageNotFoundException as infe:
-            print(infe)
+            self.screen_w = pg.size()[0]
+        except (Exception,) as e:
+            self.error_handling(e)
 
-    def firefox_one_drag(self):
-        pg.moveTo(self.firefox_left, self.firefox_top)
-        pg.dragTo(0, 0, button='left')
-        pg.mouseInfo()
+    def app_open(self, app_name: str) -> None:
+        """
+        Opens the specified application via the Start Menu.
 
-    @staticmethod
-    def firefox_open_two():
-        print('hi')
-        pg.hotkey('ctrl', 'n')
-        print('hi')
+        :param app_name: The name of the application.
+        :type app_name: str
+
+        :return: None
+        """
+        try:
+            pg.press('win')
+            time.sleep(1)
+            pg.write(app_name)
+            time.sleep(1)
+            pg.press('enter')
+            time.sleep(3)
+        except (Exception,) as e:
+            self.error_handling(e)
+
+    def app_locate(self, image_file_name: str) -> None:
+        """
+        Locates the specified image on the screen. User must crop a screenshot an image
+         representing the upper-left part of the app to identify the app to pyautogui.
+        Store this image in the project 'static' folder.
+
+        :param image_file_name: The file name of the image.
+        :type image_file_name: str
+
+        :return: None
+        """
+        try:
+            img_path = f"./static/{image_file_name}"
+            app_locator = pg.locateOnScreen(img_path, confidence=self.confidence)
+            self.app_left = app_locator.left + 8 + self.corner_avoid_val
+            self.app_top = app_locator.top + 8 + self.corner_avoid_val
+        except (Exception,) as e:
+            self.error_handling(e)
+
+    def right_move(self) -> int:
+        """
+        Adjusts values based on window width so window will be snug against right
+         side of computer screen.
+
+        :return: The adjusted x-coordinate.
+        :rtype: int | float
+        """
+        try:
+            window = GetForegroundWindow()
+            window_rect = GetWindowRect(window)
+            app_width = window_rect[2] - window_rect[0]
+            return self.screen_w - app_width + self.right_adjust_val
+        except (Exception,) as e:
+            self.error_handling(e)
+
+    def app_drag(self, desired_win_locations: list) -> None:
+        """
+        Moves mouse cursor to upper left of app window and drags to desired location.
+
+        :param desired_win_locations: The desired window location coordinates.
+
+        :return: None
+        """
+        try:
+            pg.moveTo(self.app_left, self.app_top)
+            x, y = desired_win_locations
+            # Increment pixel distance to turn mouse cursor from 'resizer' to 'grabber':
+            x += 8
+            y += 10
+            pg.dragTo(x, y, button='left', duration=1)
+        except (Exception,) as e:
+            self.error_handling(e)
+
+    def run_program(self) -> None:
+        """
+        Runs the program and all relevant class methods to open the application and
+         align its window.
+
+        :return: None
+        """
+        try:
+            self.screen_w_val()
+            self.app_open(self.app_name)
+            self.app_locate(self.image_file_name)
+            if self.align_right:
+                self.desired_win_loc[0] += self.right_move()
+            self.app_drag(self.desired_win_loc)
+        except (Exception,) as e:
+            self.error_handling(e)
 
 
+# Instantiate and run for dual Firefox windows in the developers favorite config:
 if __name__ == '__main__':
-    open_firefox_windows = OpenFirefoxWindows()
-    open_firefox_windows.get_monitor_dimensions()
-    open_firefox_windows.firefox_one_open()
-    open_firefox_windows.firefox_one_locate()
-    open_firefox_windows.firefox_one_drag()
-    open_firefox_windows.firefox_open_two()
-    print(open_firefox_windows.firefox_left)
-    print(open_firefox_windows.firefox_top)
+    try:
+        open_and_align_01 = OpenAndAlign(app_name='firefox',
+                                         image_file_name='firefox_locate.png')
+
+        open_and_align_02 = OpenAndAlign(app_name='firefox',
+                                         image_file_name='firefox_locate.png',
+                                         align_right=True)
+    except (Exception,) as e_:
+        print(f"Unexpected error instantiating OpenAndAlign class: {e_}. Exiting...")
+        sys.exit(1)
